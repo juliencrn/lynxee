5914473cc48d640cb979a29e4c0fa76d6d383ab2
anis <anishezam@gmail.com>
2022-02-13 17:59:28 +0100

    add upgrade contract feature

---
+++ bin/update.sh
@@ -0,0 +1,18 @@
+SC_ADDRESS="erd1qqqqqqqqqqqqqpgqwtya9fwg62am9smuy2p7x50q4jh5y3tfjg9q7lqesj"
+ROYALTIES="0x31303030" # 1000 means 10%
+JSON_CID="0x62616679626569657762667779326333337a7a726e367535377a36796d6e69346a697864736372796a376a6f767975696b6e736b6c667162346e34" # bafybeiewbfwy2c33zzrn6u57z6ymni4jixdscryj7jovyuiknsklfqb4n4
+IMAGE_CID="0x626166796265696466796734746b78617a637269683365616f6370776e346d363776796863756f6372756a77706c6536796a6f6c786b746e69716d" # bafybeidfyg4tkxazcrih3eaocpwn4m67vyhcuocrujwple6yjolxktniqm
+TAGS="0x746167312c74616732" # tag1,tag2
+
+
+erdpy --verbose \
+    contract upgrade ${SC_ADDRESS} \
+    --pem="wallets/owner/owner.pem" \
+    --project="."\
+    --gas-limit=100000000 \
+    --arguments 1000 ${TAGS} \
+    --send \
+    --outfile="deploy-devnet.interaction.json" \
+    --proxy="https://devnet-gateway.elrond.com" \
+    --recall-nonce \
+    --chain="D"
